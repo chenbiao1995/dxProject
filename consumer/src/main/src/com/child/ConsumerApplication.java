2ae4c797924eb6eb40cb5c6d3c75423c1e53b2a5
chenbiao <chenbiao@msunsoft.com>
2021-01-11 23:29:20 +0800

    add:服务监控

--- consumer/src/main/src/com/child/ConsumerApplication.java
+++ consumer/src/main/src/com/child/ConsumerApplication.java
@@ -1,8 +1,12 @@
 package com.child;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import ribbonConfig.MyProbabilityRandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +17,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+//加入dashboard监控必须加这个
+@EnableCircuitBreaker
 public class ConsumerApplication {
 	public static void main(String[] args) {
 		try {
@@ -30,5 +36,15 @@ public class ConsumerApplication {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet() ;
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return  registrationBean;
 	}
 }
